a3620c1b8e722f27b93deaf6165d4b795bae4e40
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-26 10:04:26 -0500

    set up useState for csv data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
+
+import { useState } from "react";
 
 export default function Home() {
+
+  const [data, setData] = useState<unknown[]>([])
   return (
     <>
     <h1>Parse CSV Data</h1>
