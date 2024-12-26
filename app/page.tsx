c5512b7da80064d81f54fe96acc718e6c9424a99
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-26 10:22:23 -0500

    imported from useFetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 
 import { useState } from "react";
 
-export default function Home() {
+import useFetch from "./hooks/useFetch";
 
+export default function Home() {
+  
   const [data, setData] = useState<unknown[]>([])
+  const {fetchCsvdata} = useState();
+  
   return (
     <>
     <h1>Parse CSV Data</h1>
