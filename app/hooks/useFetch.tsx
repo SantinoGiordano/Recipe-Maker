f9647e482c1042fa25c53cf8cf80f3afd1f8e257
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-26 10:13:44 -0500

    created logic for the fetch to take data, read it and decode it

--- app/hooks/useFetch.tsx
+++ app/hooks/useFetch.tsx
@@ -1,7 +1,16 @@
 type Callback = () => void;
 
 const useFetch = () => {
-  const fetchCsvdata = async (filePath: string, callback: Callback) => {};
+  const fetchCsvdata = async (filePath: string, callback: Callback) => {
+    const response = await fetch(filePath);
+    const reader = response.body!.getReader;
+    const result = await reader.read()
+    const decoder = new TextDecoder('utf-8');
+    const csvString = decoder.decode(result.value!)
+  };
+
+
+
 
   return { fetchCsvdata };
 };
