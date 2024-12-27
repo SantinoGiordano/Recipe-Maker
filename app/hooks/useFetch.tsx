64e360dc51183ea3f306288e6fa44077d94adba4
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-27 16:20:35 -0500

    added get reader () to make it a function and invoke it

--- app/hooks/useFetch.tsx
+++ app/hooks/useFetch.tsx
@@ -1,23 +1,21 @@
-import Papa from 'papaparse'
+import Papa from "papaparse";
 
-type Callback = (data:any) => void;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Callback = (data: any) => void;
 
 const useFetch = () => {
   const fetchCsvdata = async (filePath: string, callback: Callback) => {
     const response = await fetch(filePath);
-    const reader = response.body!.getReader;
-    const result = await reader.read()
-    const decoder = new TextDecoder('utf-8');
-    const csvString = decoder.decode(result.value!)
-    const {data} = Papa.parse(csvString, {
-        header: true,
-        dynamicTyping: true
-    })
-    callback(data)
+    const reader = response.body!.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder("utf-8");
+    const csvString = decoder.decode(result.value!);
+    const { data } = Papa.parse(csvString, {
+      header: true,
+      dynamicTyping: true,
+    });
+    callback(data);
   };
-
-
-
 
   return { fetchCsvdata };
 };
