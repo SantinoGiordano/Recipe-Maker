dfd413225feb43169833939f3e5bc8db49c2367e
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-28 12:12:19 -0500

    changed nav bar to have dropmdown menu, with stastes for transitioning

--- app/componets/Nav.tsx
+++ app/componets/Nav.tsx
@@ -1,24 +1,66 @@
-// components/Navbar.tsx
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
 const Navbar: React.FC = () => {
+  // State to track if dropdown is open
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
-    <nav className="bg-blue-600 p-10 fixed min-w-full">
-      <div className=" mx-auto flex justify-between items-center">
+    <nav className="bg-blue-600 p-10 fixed w-full z-10">
+      <div className="mx-auto flex justify-between items-center">
         <Link href="/" className="text-white text-4xl font-bold">
-         MySite
+          MySite
         </Link>
-        <div className="space-x-8">
-          <Link href="/" className="font-thin text-2xl text-white hover:text-gray-300">
+        <div className="space-x-8 flex items-center">
+          <Link
+            href="/"
+            className="font-thin text-2xl text-white hover:text-gray-300"
+          >
             Home
           </Link>
-          <Link href="/about" className="font-thin text-2xl text-white hover:text-gray-300">
-           About
+          <Link
+            href="/about"
+            className="font-thin text-2xl text-white hover:text-gray-300"
+          >
+            About
           </Link>
-          <Link href="/recipes" className="font-thin text-2xl text-white hover:text-gray-300">
-           Recipes
-          </Link>
-          <Link href="/contact" className="font-thin text-2xl text-white hover:text-gray-300">
+
+          {/* Recipes Dropdown */}
+          <div
+            className="relative"
+            onMouseEnter={() => setDropdownOpen(true)}
+            onMouseLeave={() => setDropdownOpen(false)}
+          >
+            <Link
+              href="/recipes"
+              className="font-thin text-2xl text-white hover:text-gray-300"
+            >
+              Recipes
+            </Link>
+            {/* Dropdown Menu */}
+            {dropdownOpen && (
+              <div className="absolute left-0 bg-blue-500 p-2 w-40">
+                <Link
+                  href="/recipes/full-dish"
+                  className="block text-white text-lg px-4 py-2 hover:bg-blue-600 rounded"
+                >
+                  Full Dish
+                </Link>
+                <Link
+                  href="/recipes/ingredients"
+                  className="block text-white text-lg px-4 py-2 hover:bg-blue-600 rounded"
+                >
+                  Ingredients
+                </Link>
+              </div>
+            )}
+          </div>
+
+          <Link
+            href="/contact"
+            className="font-thin text-2xl text-white hover:text-gray-300"
+          >
             Contact
           </Link>
         </div>
