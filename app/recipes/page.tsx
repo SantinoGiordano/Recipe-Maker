3101e1f683903a8c3f49edb8b7d341541fa336ef
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-28 10:31:17 -0500

    changed all the styling

---
+++ app/recipes/page.tsx
@@ -0,0 +1,29 @@
+import food from '@/data/food.json'
+
+export default function Recipes() {
+    return (
+      <>
+        {food.map((data) => (
+          <div key={data.id} className="pl-10 p-6">
+            <h3 className="text-3xl font-bold">{data.name}</h3>
+            <div>
+              <strong className="text-base">Ingredients:</strong>
+              <p className="text-sm text-gray-500 max-w-[600px]">
+                {data.ingredients.join(', ')}
+              </p>
+            </div>
+            <div>
+              <strong className="text-base">Steps:</strong>
+              <ol className="list-decimal pl-6 max-w-[600px]">
+                {data.steps.map((step, index) => (
+                  <li key={index} className="text-base">{step.description}</li>
+                ))}
+              </ol>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  }
+  
+ 
