1e3b73c1930b559c95ce6db589e2594b288f08ff
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-28 10:31:44 -0500

    created nav bar for ui

---
+++ app/componets/Nav.tsx
@@ -0,0 +1,30 @@
+// components/Navbar.tsx
+import Link from 'next/link';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-blue-600 p-10">
+      <div className=" mx-auto flex justify-between items-center">
+        <Link href="/" className="text-white text-4xl font-bold">
+         MySite
+        </Link>
+        <div className="space-x-8">
+          <Link href="/" className="font-thin text-2xl text-white hover:text-gray-300">
+            Home
+          </Link>
+          <Link href="/about" className="font-thin text-2xl text-white hover:text-gray-300">
+           About
+          </Link>
+          <Link href="/recipes"  className="font-thin text-2xl text-white hover:text-gray-300">
+           Recipes
+          </Link>
+          <Link href="/contact"  className="font-thin text-2xl text-white hover:text-gray-300">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
