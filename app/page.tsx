690a20191e5b8fe13e0be05ba6c6524692c894e7
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-28 10:08:09 -0500

    pulled a 180 and changed the csv format to be json

--- app/page.tsx
+++ app/page.tsx
@@ -1,110 +1,28 @@
-'use client'
-import React, { useState } from "react";
-import Papa from "papaparse";
+import food from '@/data/food.json';
 
-// Allowed extensions for input file
-const allowedExtensions = ["csv"];
-
-const App = () => {
-
-    // This state will store the parsed data
-    const [data, setData] = useState([]);
-
-    // It state will contain the error when
-    // correct file extension is not used
-    const [error, setError] = useState("");
-
-    // It will store the file uploaded by the user
-    const [file, setFile] = useState("");
-
-    // This function will be called when
-    // the file input changes
-    const handleFileChange = (e) => {
-        setError("");
-
-        // Check if user has entered the file
-        if (e.target.files.length) {
-            const inputFile = e.target.files[0];
-
-            // Check the file extensions, if it not
-            // included in the allowed extensions
-            // we show the error
-            const fileExtension =
-                inputFile?.type.split("/")[1];
-            if (
-                !allowedExtensions.includes(fileExtension)
-            ) {
-                setError("Please input a csv file");
-                return;
-            }
-
-            // If input type is correct set the state
-            setFile(inputFile);
-        }
-    };
-    const handleParse = () => {
-    
-        // If user clicks the parse button without
-        // a file we show a error
-        if (!file) return alert("Enter a valid file");
-
-        // Initialize a reader which allows user
-        // to read any file or blob.
-        const reader = new FileReader();
-
-        // Event listener on reader when the file
-        // loads, we parse it and set the data.
-        reader.onload = async ({ target }) => {
-            const csv = Papa.parse(target.result, {
-                header: true,
-            });
-            const parsedData = csv?.data;
-            const rows = Object.keys(parsedData[0]);
-
-            const columns = Object.values(parsedData[0]);
-            const res = rows.reduce((acc, e, i) => {
-                return [...acc, [[e], columns[i]]];
-            }, []);
-            console.log(res);
-            setData(res);
-        };
-        reader.readAsText(file);
-    };
-
-    return (
-        <div className="App">
-            <h1 className="geeks">GeeksforGeeks</h1>
-            <h3>Read CSV file in React</h3>
-            <div className="container">
-                <label
-                    htmlFor="csvInput"
-                    style={{ display: "block" }}
-                >
-                    Enter CSV File
-                </label>
-                <input
-                    onChange={handleFileChange}
-                    id="csvInput"
-                    name="file"
-                    type="File"
-                />
-                <div>
-                    <button onClick={handleParse}>
-                        Parse
-                    </button>
-                </div>
-                <div style={{ marginTop: "3rem" }}>
-                    {error
-                        ? error
-                        : data.map((e, i) => (
-                              <div key={i} className="item">
-                                  {e[0]}:{e[1]}
-                              </div>
-                          ))}
-                </div>
-            </div>
+export default function Home() {
+  return (
+    <>
+      {food.map((data) => (
+        <div key={data.id} className="pl-10 p-6">
+          <h3 className="text-3xl font-bold">{data.name}</h3>
+          <div>
+            <strong className="text-base">Ingredients:</strong>
+            <p className="text-sm text-gray-500 max-w-[600px]">
+              {data.ingredients.join(', ')}
+            </p>
+          </div>
+          <div>
+            <strong className="text-base">Steps:</strong>
+            <ol className="list-decimal pl-6 max-w-[600px]">
+              {data.steps.map((step, index) => (
+                <li key={index} className="text-base">{step.description}</li>
+              ))}
+            </ol>
+          </div>
         </div>
-    );
-};
+      ))}
+    </>
+  );
+}
 
-export default App;
