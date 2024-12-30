b03fb4020fff37529bd022efa0a9ff1b4e130776
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-30 15:15:05 -0500

    remapped some data

--- app/(root)/recipes/full-dish/page.tsx
+++ app/(root)/recipes/full-dish/page.tsx
@@ -1,29 +1,46 @@
-import food from '@/data/food.json'
+import food from '@/data/food.json';
 
 export default function Recipes() {
-    return (
-      <>
-      Full dish
-        {food.map((data) => (
-          <div key={data.id} className="pl-10 p-6">
-            <p className="text-3xl font-bold">{data.name}</p>
-            <div>
-              <strong className="text-base">Ingredients:</strong>
-              <p className="text-sm text-gray-500 max-w-[600px]">
-                {data.ingredients.join(', ')}
-              </p>
-            </div>
-            <div>
-              <strong className="text-base">Steps:</strong>
-              <ol className="list-decimal pl-6 max-w-[600px]">
-                {data.steps.map((step, index) => (
-                  <li key={index} className="text-base">{step.description}</li>
-                ))}
-              </ol>
-            </div>
+  return (
+    <>
+      <div
+        className="bg-panSearing p-5  bg-cover bg-center bg-fixed"
+        style={{
+          backgroundImage: "url('/foodOnTable.jpg')", // Ensure the correct path to your image
+        }}
+      >
+        <h2 className="pt-5 text-4xl font-bold text-center mb-8 text-white">Full Dish</h2>
+        <div className="flex justify-center items-center px-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 max-w-screen-xl w-full">
+            {food.map((data) => (
+              <div
+                key={data.id}
+                className="bg-white shadow-xl rounded-lg overflow-hidden border border-gray-300 w-[400px]"
+              >
+                <div className="p-8">
+                  <p className="text-3xl font-bold text-center mb-6">{data.name}</p>
+                  <div className="mb-6">
+                    <strong className="text-lg">Ingredients:</strong>
+                    <p className="text-md text-gray-600 max-w-[600px] mx-auto">
+                      {data.ingredients.join(', ')}
+                    </p>
+                  </div>
+                  <div>
+                    <strong className="text-lg">Steps:</strong>
+                    <ol className="list-decimal pl-8 text-md">
+                      {data.steps.map((step, index) => (
+                        <li key={index} className="text-gray-700 mb-3">
+                          {step.description}
+                        </li>
+                      ))}
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </>
-    );
-  }
-  
+        </div>
+      </div>
+    </>
+  );
+}
