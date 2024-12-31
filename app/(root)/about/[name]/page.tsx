909262c21f9178a6c2eb8958d69c91d643e3c3f6
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-31 10:51:05 -0500

    created a dynamic page for all my dished when there clicked

---
+++ app/(root)/about/[name]/page.tsx
@@ -0,0 +1,14 @@
+// interface Params{
+//  id:string
+// }
+
+export default function DishDetails({params}:{
+    params:{name:string}
+}){
+    return (
+    <div className="p-5">
+        You chose: {params.name}
+    </div>
+  )
+}
+
