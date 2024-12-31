8f89677da883c65e171eaa495b18958d4082bcc9
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-31 11:08:43 -0500

    Currently can not fetch data as i need an api fetch call but will do that soon

--- app/(root)/about/[name]/page.tsx
+++ app/(root)/about/[name]/page.tsx
@@ -1,13 +1,11 @@
-// interface Params{
-//  id:string
-// }
+
 
 export default function DishDetails({params}:{
     params:{name:string}
 }){
     return (
     <div className="p-5">
-        You chose: {params.name}
+        You chose {params.name}
     </div>
   )
 }
