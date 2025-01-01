6bdd077923bfa0d88d209f00805be351eccf3d6c
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-01 17:34:41 -0500

    greated get, and changed status 201

--- app/api/getRecipes/route.ts
+++ app/api/getRecipes/route.ts
@@ -1,10 +1,10 @@
+
 import food from '@/data/food.json'
 
 export async function GET() {
     return new Response(JSON.stringify(food), {
-        status: 200,
+        status: 201,
         headers: {
             'Content-Type': 'application/json',
         },
-    });
-}
+    })}
