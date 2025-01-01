6a31252eee667598eedc02635f3ff4ad5e337e27
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-01 17:36:43 -0500

    created new post request

---
+++ app/api/postRecipes/route.tsx
@@ -0,0 +1,19 @@
+
+import food from '@/data/food.json'
+
+export async function POST(request:Request){
+
+    const dish = await request.json()
+    const newFood = {
+        id:food.length +1,
+        name: dish.name
+    }
+    food.push(newFood)
+    
+    return new Response(JSON.stringify(newFood), {
+        status: 201,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+}
