d2149c7960b16488d559fe4093ebfab380a6e075
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-02 11:36:38 -0500

    created a simple ts loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Laoding(){
+    return(
+        <div className="flex justify-center items-center min-h-screen">
+        <div className="bg-white p-8 text-center max-w-lg mx-auto shadow-lg rounded-lg">
+          <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+            Were raching your page, 
+          </h1>
+          <p className="text-lg text-gray-600 mb-6">
+          Just wait one sec . . .
+          </p>
+        </div>
+      </div>
+    
+    )
+}
