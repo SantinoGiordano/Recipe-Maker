48acf3c2185ee9fe9edc1c4da28fed5caab7ae9d
SantinoGiordano <santinogiordano13@gmail.com>
2025-01-03 10:45:23 -0500

    via post man i now have all crud elements, this one was delete

--- app/api/patchRecipes/[id]/route.ts
+++ app/api/patchRecipes/[id]/route.ts
@@ -1,35 +1,50 @@
 import food from '@/data/food.json'
+// import { NextRequest } from 'next/server'
 
 
-export async function GET( 
-    _request: Request, 
+// export async function GET( 
+//     _request: Request, 
+//     {params} : {params:{id:string}}
+// ){
+//     const foods = food.find( 
+//         (foods) => foods.id === parseInt(params.id)
+//     )
+//     return new Response(JSON.stringify(foods), {
+//         status: 200,
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     })}
+
+
+export async function PATCH(
+    request:Request,
     {params} : {params:{id:string}}
 ){
-    const foods = food.find( 
-        (foods) => foods.id === parseInt(params.id)
+
+    const body = await request.json()
+    const { name } = body
+    const index = food.findIndex(
+        food =>  food.id === parseInt(params.id)
     )
-    return new Response(JSON.stringify(foods), {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })}
+    food[index].name = name;
+return new Response(JSON.stringify(food[index]), {
+    status: 200,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})}
 
-
-    export async function PATCH(
-        request:Request,
-        {params} : {params:{id:string}}
-    ){
+export async function DELETE(
+    request:Request,
+    {params} : {params:{id:string}}
+){
     
-        const body = await request.json()
-        const { name } = body
-        const index = food.findIndex(
-           food =>  food.id === parseInt(params.id)
-        )
-        food[index].name = name;
-    return new Response(JSON.stringify(food[index]), {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })}
+    const index = food.findIndex(
+        (food) =>  food.id === parseInt(params.id)
+    )
+    const deletedFood = food[index]
+    food.splice(index,1)
+    return Response.json(deletedFood)
+
+}
